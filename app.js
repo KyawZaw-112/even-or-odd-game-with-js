d4afd0666918bbd4c998fc678c6c65f4c26175ab
KyawZaw-112 <winkyawzaw660@gmail.com>
2023-10-23 22:08:02 +0630

    fix: game logic

--- app.js
+++ app.js
@@ -5,101 +5,36 @@ const exitBtn = document.querySelector("#exitBtn");
 const mainscreen = document.querySelector(".main-screen");
 const guideBtn = document.querySelector("#guideBtn");
 
-//for game
-
-let number = 0;
-let score = 0;
-
 //DOM elements
 
 const evenBtn = document.querySelector("#evenBtn");
 const oddBtn = document.querySelector("#oddBtn");
 const scoreBoard = document.querySelector("#userScore");
 let numberStart = document.querySelector("#number");
-// const nextBtn = document.querySelector("#nextBtn");
-// let ramNum = Math.floor(Math.random() *10);
 
-//function for wrapper screen
-scoreBoard.innerHTML = score;
 
-numberStart.textContent = start();
-startBtn.addEventListener("click", () => {
-	wrapperScrren.style.display = "none";
-	mainscreen.style.display = "flex";
-});
 
-exitBtn.addEventListener("click", () => {
-	Swal.fire({
-		title: "Are you sure?",
-		text: "Just Play It's a game, Bro!!!",
-		icon: "question",
-		showCancelButton: true,
-		confirmButtonColor: "#f00",
-		cancelButtonColor: "#3085d6",
-		confirmButtonText: "Yes, I exit!",
-	}).then((result) => {
-		if (result.isConfirmed) {
-			Swal.fire("EXIT!", "Don't leave me alone.", "success");
-			exit();
-		}
-	});
-});
+// startBtn.addEventListener("click", () => {
+// 	wrapperScrren.style.display = "none";
+// 	mainscreen.style.display = "flex";
+// });
 
-function start() {
-	ramNum = Math.floor(Math.random() * 100);
-	numberStart.innerText = ramNum;
-	console.log(ramNum);
-	return ramNum;
-}
-
-function ev() {
-	return "even";
-}
-
-function od() {
-	return "odd";
-}
-
-// nextBtn.addEventListener("click", function() {
-// 	start();
-// })
-
-evenBtn.addEventListener("click", function () {
-	let even = ev();
-	start();
-	if (check() === even) {
-		score++;
-		scoreBoard.innerText = score;
-		console.log("true it's even");
-	} else {
-		score--;
-		scoreBoard.innerText = score;
-		console.log("false it's odd");
-	}
-});
-
-oddBtn.addEventListener("click", function () {
-	let odd = od();
-	start();
-	if (check() === odd) {
-		score++;
-		scoreBoard.innerText = score;
-		console.log("true it's odd");
-	} else {
-		score--;
-		scoreBoard.innerText = score;
-		console.log("false it's even");
-	}
-});
-
-function check() {
-	let startnumber = numberStart.innerText;
-	if (startnumber % 2 === 0) {
-		return "even";
-	} else {
-		return "odd";
-	}
-}
+// exitBtn.addEventListener("click", () => {
+// 	Swal.fire({
+// 		title: "Are you sure?",
+// 		text: "Just Play It's a game, Bro!!!",
+// 		icon: "question",
+// 		showCancelButton: true,
+// 		confirmButtonColor: "#f00",
+// 		cancelButtonColor: "#3085d6",
+// 		confirmButtonText: "Yes, I exit!",
+// 	}).then((result) => {
+// 		if (result.isConfirmed) {
+// 			Swal.fire("EXIT!", "Don't leave me alone.", "success");
+// 			exit();
+// 		}
+// 	});
+// });
 
 function exit() {
 	score = 0;
@@ -112,7 +47,7 @@ function exit() {
 guideBtn.addEventListener("click", function () {
 	Swal.fire({
 		title: "Guide",
-		text: "The game start You can see the number but you can guess first the number.You guess next number is even or odd. If you can guess next number is right then you get one point but if you wrong , you lose your original point",
+		text: "The game start You can see the number but you can guess first the number.You guess next number is even or odd. If you can guess next number is right then you get one point but if you wrong , you lose from your original points",
 		textOne: "1. You guess next number is even or odd",
 		showClass: {
 			popup: "animate__animated animate__bounceInDown",
@@ -122,3 +57,42 @@ guideBtn.addEventListener("click", function () {
 		},
 	});
 });
+
+//for game
+let number = 0;
+let score = 0;
+
+//function for wrapper screen
+scoreBoard.innerHTML = score;
+
+//game functionality
+let randomNumber  = Math.floor(Math.random() * 10);
+console.log(randomNumber);
+
+function autoGenerateNumber() {
+	let randomFunction = randomNumber;
+	console.log(randomFunction);
+	return randomFunction;
+}
+console.log(autoGenerateNumber());
+
+const checkEvenOrOdd = () => {
+	let checkNumber = randomNumber;
+	if (checkNumber % 2 === 0) {
+		console.log("even " + checkNumber);
+		return "even";
+	} else {
+		console.log("odd " + checkNumber);
+		return "odd";
+	}
+}
+
+let evenOrOdd = checkEvenOrOdd();
+
+
+
+
+
+
+
+
